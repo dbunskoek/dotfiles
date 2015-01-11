98766ca482ffc66c8bbd8fb290095867a3381352
Dennis Bunskoek <dbunskoek@leukeleu.nl>
2015-01-12 00:52:44 +0100

    Cleaned up .dotfiles

--- symlink_all.sh
+++ symlink_all.sh
@@ -9,12 +9,8 @@ echo "symlinking .bash_prompt"
 echo "symlinking .aliases"
 [ ! -f ~/.aliases ] && ln -s ~/.dotfiles/.aliases ~/.aliases
 
-echo "symlinking .gitignore"
-[ ! -f ~/.gitignore ] && ln -s ~/.dotfiles/.gitignore ~/.gitignore
+echo "symlinking .gitignore_global"
+[ ! -f ~/.gitignore ] && ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
 
 echo "symlinking .gitconfig"
 [ ! -f ~/.gitconfig ] && ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-
-echo "symlinking Sublime Text 2 preferences and keymap"
-[ ! -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings ] && ln -s ~/.dotfiles/Sublime\ Text\ 2/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
-[ ! -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap ] && ln -s ~/.dotfiles/Sublime\ Text\ 2/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
